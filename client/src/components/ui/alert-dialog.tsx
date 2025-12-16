b430d2238b8f1770aebf33817204701ade54d8de
speakaiagency-hue <speakai.agency@gmail.com>
2025-12-15 22:25:22 -0300

    Update alert-dialog.tsx

--- client/src/components/ui/alert-dialog.tsx
+++ client/src/components/ui/alert-dialog.tsx
@@ -31,19 +31,24 @@ const AlertDialogContent = React.forwardRef<
     <AlertDialogOverlay />
     <AlertDialogPrimitive.Content
       ref={ref}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
         className
       )}
       {...props}
     >
-      {/* Garantindo acessibilidade: sempre renderiza Title e Description se não forem passados */}
+      {/* Elementos obrigatórios para acessibilidade */}
       <AlertDialogHeader>
-        <AlertDialogTitle className="sr-only">Título do diálogo</AlertDialogTitle>
-        <AlertDialogDescription className="sr-only">
+        <AlertDialogTitle id="alert-dialog-title" className="sr-only">
+          Título do diálogo
+        </AlertDialogTitle>
+        <AlertDialogDescription id="alert-dialog-description" className="sr-only">
           Descrição do diálogo
         </AlertDialogDescription>
       </AlertDialogHeader>
+
       {children}
     </AlertDialogPrimitive.Content>
   </AlertDialogPortal>
@@ -97,8 +102,7 @@ const AlertDialogDescription = React.forwardRef<
     {...props}
   />
 ))
-AlertDialogDescription.displayName =
-  AlertDialogPrimitive.Description.displayName
+AlertDialogDescription.displayName = AlertDialogPrimitive.Description.displayName
 
 const AlertDialogAction = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Action>,
